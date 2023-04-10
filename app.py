efa499305d536af05a9895d1282f4518e4145d02
riomonet <arizablozki@gmail.com>
2023-04-10 08:53:23 -0400

    move responses list into a flask session variable

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import personality_quiz
 
@@ -18,17 +18,24 @@ x = 0
 def main():
     return render_template('start.html',title=quiz.title, instruction=quiz.instructions)
 
+@app.route('/reset_data', methods=["POST"])
+def reset():
+    global x
+    session['responses'] = []
+    x = 0
+    return redirect('/questions/0')
+
 
 # use form template to display questions and options for answers
 @app.route('/questions/<int:q_num>')
 def question(q_num):
+
     if x >= number_of_questions:
         flash("You can only take the survey once, you can't change your answers!!")
         return redirect('/thankyou')
     elif q_num != x:
         flash("Pardon me, you skipped a question, please answer THIS question")
         q_num = x
-                                                                # used this before return redirect(f'/questions/{q_num}')
     return render_template("question.html", q=questions[q_num].question, choices=questions[q_num].choices, item = q_num)
 
 
@@ -38,7 +45,10 @@ def answer(q_num):
     global x    
     q_num += 1
     x += 1
-    responses.append(request.form['ans'])
+#    responses.append(request.form['ans'])
+    res = session['responses']
+    res.append(request.form['ans'])
+    session['responses'] = res
 
     if q_num < number_of_questions:
         return redirect(f'/questions/{q_num}')
@@ -48,5 +58,5 @@ def answer(q_num):
 
 @app.route('/thankyou')
 def thankyou():
-        return render_template('thankyou.html',responses=responses)
+        return render_template('thankyou.html')
     
